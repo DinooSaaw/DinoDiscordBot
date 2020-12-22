e89462ac4e89ed6f35055e5e269c2d2f9ce8b7ca
DinooSaaw <57213778+DinooSaaw@users.noreply.github.com>
2020-12-23 01:41:10 +1100

    Created Coinflip.js

---
+++ src/Commands/Fun/coinflip.js
@@ -0,0 +1,33 @@
+const { MessageEmbed, MessageAttachment } = require('discord.js')
+const Command = require('../../Structures/Command');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+		super(...args, {
+      aliases: ['cf'],
+			description: 'Coinflip',
+			category: 'Fun',
+		});
+	}
+
+	// eslint-disable-next-line no-unused-vars
+	async run(message) {
+        let random = (Math.floor(Math.random() * Math.floor(2)));
+        const heads = new MessageEmbed()
+        .setImage('https://media1.tenor.com/images/26ceb9ab4b5b070f7b0257f8f4e0c80f/tenor.gif?itemid=13943298')
+        .setTitle('Heads!')
+        let Tails = new MessageEmbed()
+        .setTitle('Tails!')
+        .setImage('https://media1.tenor.com/images/6d7a3ee293c118b90205aa2fb3a7f740/tenor.gif?itemid=13943297')
+
+    if(random === 0) {
+      message.delete()
+      message.channel.send(heads);
+    }
+    else {
+      message.delete()
+      message.channel.send(Tails);
+    }
+}
+}
