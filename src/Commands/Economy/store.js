fc95e0fe3f619d5086eaff690c32b9f091729f33
DinooSaaw <57213778+DinooSaaw@users.noreply.github.com>
2021-02-10 23:33:42 +1100

    Added 'Riot Set' which adds Glock-19, Riot_Shield

--- src/Commands/Economy/store.js
+++ src/Commands/Economy/store.js
@@ -32,7 +32,7 @@ module.exports = class extends Command {
 			])
 			.addField('Battle Set', [
 				`**❯ $50,000:** Diamond Set (Id:4)`,
-				`**❯ $90,000:** ~~Gun Set~~ (Id:5)`,
+				`**❯ $90,000:** Riot  Set (Id:5)`,
 				`**❯ $500,000:** ~~Anime Set~~ (Id:6)`,
 			])
 			.setFooter(`Requested by ${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
@@ -84,6 +84,18 @@ module.exports = class extends Command {
 						if (user.money < 100000) return message.reply(`You dont have enough money`)
 						message.reply("You Want To Buy (ID6)!");
 						ID6()
+					} else if (message.content == "!info 1") {
+						InfoID1()
+					} else if (message.content == "!info 2") {
+						InfoID2()
+					} else if (message.content == "!info 3") {
+						InfoID3()
+					} else if (message.content == "!info 4") {
+						InfoID4()
+					} else if (message.content == "!info 5") {
+						InfoID5()
+					} else if (message.content == "!info 6") {
+						InfoID6()
 					}
 			})
 		}
@@ -131,8 +143,7 @@ module.exports = class extends Command {
 						message.channel.send(`${message.author.username} bought ID3`)
 					} else return	
 				})
-			}
-
+		}
 			function ID4(){
 				message.channel.send("Are You Sure /n `Yes` or `No`")
 				const collector = new MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
@@ -142,8 +153,11 @@ module.exports = class extends Command {
 						console.log(chalk.hex('70bb65')('[Transaction] ') + chalk.magenta(`[${message.author.username}] `) + chalk.bold.white(`Money: $${user.money} Bank: $${user.bank}`))
 						user.money -=  50000
 						user.Tags.push('Store:Id4')
-						if (user.Tags.includes('Iron_Sword')) {
+						if (user.Inventory.includes('Iron_Sword')) {
 							user.Inventory.pull('Iron_Sword')
+						}
+						if (user.Inventory.includes('Glock-19')) {
+							user.Inventory.pull('Glock-19')
 						}
 						user.Inventory.push('Diamond_Sword')
 						user.save()
@@ -151,62 +165,103 @@ module.exports = class extends Command {
 						message.channel.send(`${message.author.username} bought ID4`)
 					} else return	
 				})
-			}
-
+		}
+			function ID5(){
+				message.channel.send("Are You Sure /n `Yes` or `No`")
+				const collector = new MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
+				// console.log(collector)
+				collector.on('collect', message => {
+					if (message.content == "Yes") {
+						console.log(chalk.hex('70bb65')('[Transaction] ') + chalk.magenta(`[${message.author.username}] `) + chalk.bold.white(`Money: $${user.money} Bank: $${user.bank}`))
+						user.money -=  90000
+						user.Tags.push('Store:Id5')
+						if (user.Inventory.includes('Iron_Sword')) {
+							user.Inventory.pull('Iron_Sword')
+						}
+						if (user.Inventory.includes('Diamond_Sword')) {
+							user.Inventory.pull('Diamond_Sword')
+						}
+						user.Inventory.push('Glock-19')
+						if (user.Inventory.includes('Wooden_Shield')) {
+							user.Inventory.pull('Wooden_Shield')
+						}
+						user.Inventory.push('Riot_Shield')
+						user.save()
+						message.channel.bulkDelete(7)
+						message.channel.send(`${message.author.username} bought ID5`)
+					} else return	
+				})
+		}
 			function InfoID1 () {
 				Info.addField('Custom Command', [
 					`**❯ Cost:** $500,000`,
 					`**❯ ID:** 1`,
 					`**❯ Name:** Custom Command`
 				])
+				
+				message.channel.bulkDelete(7)
 				message.channel.send(Info)
-			}
-			
+		}	
 			function InfoID2 () {
 				Info.addField('Custom Command', [
 					`**❯ Cost:** $500,000`,
 					`**❯ ID:** 1`,
 					`**❯ Name:** Custom Command`
 				])
-				message.channel.send(Info)
-			}
 
+				message.channel.bulkDelete(7)
+				message.channel.send(Info)
+		}
 			function InfoID3 () {
 				Info.addField('Custom Coding Lesson', [
 					`**❯ Cost:** $100,000`,
 					`**❯ ID:** 3`,
 					`**❯ Name:** Custom Coding Lesson`
 				])
+
+				
+				message.channel.bulkDelete(7)
 				message.channel.send(Info)
-			}
+		}
 			function InfoID4 () {
 				Info.addField('Diamond Set', [
 					`**❯ Cost:** $50,000`,
 					`**❯ ID:** 4`,
 					`**❯ Name:** Diamond Set`
 				])
+				
+				message.channel.bulkDelete(7)
 				Info.addField('Unlocks', [
 					`**❯ Sword:** *Diamond Sword*`,
 					`**❯ Shield:** *Null*`,
 
 				])
 				message.channel.send(Info)
-			}
+		}
 			function InfoID5 () {
-				Info.addField('Custom Command', [
-					`**❯ Cost:** $500,000`,
-					`**❯ ID:** 1`,
-					`**❯ Name:** Custom Command`
+				Info.addField('Riot Set', [
+					`**❯ Cost:** $90,000`,
+					`**❯ ID:** 5`,
+					`**❯ Name:** Riot Set`
 				])
-				message.channel.send(Info)
-			}
+				
+				message.channel.bulkDelete(7)
+				Info.addField('Unlocks', [
+					`**❯ Sword:** *Glock-19*`,
+					`**❯ Shield:** *Riot_Shield*`,
+				])
+				message.channel.bulkDelete(7)
+				// message.channel.send(Info)
+		}
 			function InfoID6 () {
 				Info.addField('Custom Command', [
 					`**❯ Cost:** $500,000`,
 					`**❯ ID:** 1`,
 					`**❯ Name:** Custom Command`
 				])
-				message.channel.send(Info)
-			}
+
+				message.channel.bulkDelete(7)
+				// message.channel.send(Info)
 		}
+	}
 }
