638e12ae42cbc326dd3886bb454700ff318af5e5
DinooSaaw <57213778+DinooSaaw@users.noreply.github.com>
2021-03-13 21:50:08 +1100

    Added anime

---
+++ commands/Anime/smug.js
@@ -0,0 +1,34 @@
+const client = require("nekos.life");
+const { sfw } = new client();
+const { MessageEmbed } = require('discord.js');
+const DBUser = require("../../Mongoose/Schema/user");
+const DBGuild = require("../../Mongoose/Schema/Guild");
+
+
+module.exports = {
+  name: "smug",
+  description:
+  "smug face on!",
+  usage: "baka <user>",
+	aliases: ['smugface'],
+    category: "Anime",
+    run: async (client, message, args) => {
+		const member = message.mentions.members.last() || message.member;
+		let user = await DBUser.findOne({ id: member.user.id, Username: member.user.username });
+		if (!user) return message.channel.send(`**${member.user.username}** isnt in the system`)
+		let guild = await DBGuild.findOne({ GuildId: message.guild.id})
+		if (!guild) return message.channel.send(`Something when wrong!`)
+    let slap = await sfw.smug();
+    let member = message.mentions.members.first();
+    const args = message.content.trim().split(/ +/g);
+    //if (!args[1]) return message.reply("mention someone!")
+
+    if (member) {
+      let embed = new MessageEmbed()
+        //.setTitle(`Pat!`)
+        .setColor(guild.embedColor)
+        .setImage(slap.url);
+      message.channel.send(embed);
+    } else console.log("error");
+  }
+};
---
+++ commands/Anime/tickle.js
@@ -0,0 +1,35 @@
+const client = require("nekos.life");
+const { sfw } = new client();
+const { MessageEmbed } = require('discord.js');
+const DBUser = require("../../Mongoose/Schema/user");
+const DBGuild = require("../../Mongoose/Schema/Guild");
+
+
+module.exports = {
+  name: "tickle",
+  description:
+  "Tickle time!",
+  usage: "tickle <user>",
+	aliases: ['tickle'],
+    category: "Anime",
+    run: async (client, message, args) => {
+		const member = message.mentions.members.last() || message.member;
+		let user = await DBUser.findOne({ id: member.user.id, Username: member.user.username });
+		if (!user) return message.channel.send(`**${member.user.username}** isnt in the system`)
+		let guild = await DBGuild.findOne({ GuildId: message.guild.id})
+		if (!guild) return message.channel.send(`Something when wrong!`)
+    let slap = await sfw.tickle();
+    let member = message.mentions.members.first();
+    const args = message.content.trim().split(/ +/g);
+    if (!args[1]) return message.reply("mention someone!")
+
+    if (member) {
+      let embed = new MessageEmbed()
+        .setTitle(`${message.guild.member(member).displayName}`)
+        .setColor(guild.embedColor)
+        .setImage(slap.url);
+        message.channel.send(embed);
+        console.log(message.channel)
+    } else console.log("error");
+  }
+};
