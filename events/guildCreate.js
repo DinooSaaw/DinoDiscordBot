95d1c3bfd67655d3a6e47d2d8fa588120efdbce3
DinooSaaw <57213778+DinooSaaw@users.noreply.github.com>
2021-03-13 23:40:30 +1100

    Update

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -4,20 +4,20 @@ const DBGuild = require("../Mongoose/Schema/Guild");
 module.exports.run = async (client, guild) => { 
 	if (!process.env.webhookID) throw new Error(`webhookID is needed`)
 	if (!process.env.webhookToken) throw new Error(`webhookID is needed`)
-	let dataguild = await DBGuild.findOne({ GuildId: message.guild.id});
+	let dataguild = await DBGuild.findOne({ GuildId: guild.id});
 	if (!dataguild) {
 		const Guild = new DBGuild({
-			_id: `Guild:${message.guild.name}`,
-			GuildName: message.guild.name,
-			Region: message.guild.region,
-			MemberCount: message.guild.memberCount,
-			GuildId: message.guild.id,
-			OwnerId: message.guild.OwnerId,
-			Owner: message.guild.Owner,
-			partnered: message.guild.partnered,
-			PremiumTier: message.guild.PremiumTier,
-			PremiumSubscriptionCount: message.guild.PremiumSubscriptionCount,
-			verified: message.guild.verified
+			_id: `Guild:${guild.name}`,
+			GuildName: guild.name,
+			Region: guild.region,
+			MemberCount: guild.memberCount,
+			GuildId: guild.id,
+			OwnerId: guild.OwnerId,
+			Owner: guild.Owner,
+			partnered: guild.partnered,
+			PremiumTier: guild.PremiumTier,
+			PremiumSubscriptionCount: guild.PremiumSubscriptionCount,
+			verified: guild.verified
 		  });
 		  Guild.save();
 		  console.log(
