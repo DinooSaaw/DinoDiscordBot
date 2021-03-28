2e0c1f5e255f8279c77502cb0f544ac0cc6fa7e1
DinooSaaw <57213778+DinooSaaw@users.noreply.github.com>
2021-03-28 15:49:20 +1100

    bug fix

--- index.js
+++ index.js
@@ -5,25 +5,11 @@ const client = new discord.Client({
   disableEveryone: true 
 });
 const Topgg = require("@top-gg/sdk");
-const express = require("express");
-
-const app = express();
 const AutoPoster = require('topgg-autoposter')
-
 const ap = AutoPoster(process.env.topggtoken, client)
-
 ap.on('posted', () => {
   console.log((`[SYSTEM] `) + `Posted stats to Top.gg!`)
 })
-
-const webhook = new Topgg.Webhook(process.env.webhookToken);
-
-app.post("/dblwebhook", webhook.middleware(), (req, res) => {
-  // req.vote will be your vote object, e.g
-  console.log(req.vote.user); // 395526710101278721 < user who voted
-});
-
-app.listen(80);
 
 mongoose.connect ( process.env.URL ,  {  useNewUrlParser : true ,  useUnifiedTopology : true  } ) ;
 mongoose.connection.on( 'connected' , ( ) => {console.log((`[DataBase] `) + ('Connected!'))})
