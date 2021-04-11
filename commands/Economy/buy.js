755746f64a41a43b9187588fc614cf680647ecaf
DinooSaaw <57213778+DinooSaaw@users.noreply.github.com>
2021-04-11 14:57:12 +1000

    update buy

--- commands/Economy/buy.js
+++ commands/Economy/buy.js
@@ -39,8 +39,8 @@ module.exports = {
         },
         { name: "\u200B", value: "\u200B" }
       )
-      .setFooter(`Buy with !buy`)
-    
+      .setFooter(`Buy with !buy`);
+
     if (!args) return message.channel.send(store);
     if (args[0] == "1" || args[0] == "one" || args[0] == "One") {
       one("Custom Guild Command", 500000);
@@ -52,7 +52,61 @@ module.exports = {
       message.channel.bulkDelete(2, true).catch(async (err) => {
         console.error(err);
       });
+      if (user.money < cost) {
+        failed = new MessageEmbed()
+          .setTitle(`${message.author.username}'s **Purchase Failed**`)
+          .setColor("RED")
+          .setDescription(
+            `This may because you don't have enough money or the money is in the bank`
+          );
+          return message.channel.send(failed)
+      }
+      let Buy = new MessageEmbed()
+        .setColor(guild.embedColor)
+        .setTitle(`${message.author.username} Has Bought ${item}`)
+        .setDescription(
+          `${message.author.username} Spent $${cost} \n Please Now State What The Command Does *include **Command Name, Description, Usage, aliases ***`
+        );
+        
+      message.channel.send(Buy);
+      const collector = new MessageCollector(
+        message.channel,
+        (m) => m.author.id === message.author.id,
+        { max: 1, time: 180000 }
+      );
+      collector.on("collect", (m) => {
+        const Webhook = new MessageEmbed()
+          .setTitle(
+            `${message.author.username}#${message.author.discriminator}(${message.author.id})'s ${item}`
+          )
+          .setFooter(`${message.channel.name} in ${message.guild.name}`)
+          .setDescription(m.content)
+          .setColor(guild.embedColor);
+        webhookClient.send(`<@!247163579424309268>`);
+        webhookClient.send(Webhook);
+      });
+      collector.on("end", (collected) => {
+        message.channel.send(
+          `Thank You. Please Have DM Open So Any Problem Can Be Fixed \n Have A Good Day`
+        );
+        user.money -= cost
+        user.save()
+      });
+    }
 
+    function two(item, cost) {
+      message.channel.bulkDelete(2, true).catch(async (err) => {
+        console.error(err);
+      });
+      if (user.money < cost) {
+        failed = new MessageEmbed()
+          .setTitle(`${message.author.username}'s **Purchase Failed**`)
+          .setColor("RED")
+          .setDescription(
+            `This may because you don't have enough money or the money is in the bank`
+          );
+          return message.channel.send(failed)
+      }
       let Buy = new MessageEmbed()
         .setColor(guild.embedColor)
         .setTitle(`${message.author.username} Has Bought ${item}`)
@@ -80,42 +134,9 @@ module.exports = {
         message.channel.send(
           `Thank You. Please Have DM Open So Any Problem Can Be Fixed \n Have A Good Day`
         );
+        user.money -= cost
+        user.save()
       });
-    }
-
-    function two(item, cost) {
-        message.channel.bulkDelete(2, true).catch(async (err) => {
-            console.error(err);
-          });
-    
-          let Buy = new MessageEmbed()
-            .setColor(guild.embedColor)
-            .setTitle(`${message.author.username} Has Bought ${item}`)
-            .setDescription(
-              `${message.author.username} Spent $${cost} \n Please Now State What The Command Does *include **Command Name, Description, Usage, aliases ***`
-            );
-          message.channel.send(Buy);
-          const collector = new MessageCollector(
-            message.channel,
-            (m) => m.author.id === message.author.id,
-            { max: 1, time: 180000 }
-          );
-          collector.on("collect", (m) => {
-            const Webhook = new MessageEmbed()
-              .setTitle(
-                `${message.author.username}#${message.author.discriminator}(${message.author.id})'s ${item}`
-              )
-              .setFooter(`${message.channel.name} in ${message.guild.name}`)
-              .setDescription(m.content)
-              .setColor(guild.embedColor);
-            webhookClient.send(`<@!247163579424309268>`);
-            webhookClient.send(Webhook);
-          });
-          collector.on("end", (collected) => {
-            message.channel.send(
-              `Thank You. Please Have DM Open So Any Problem Can Be Fixed \n Have A Good Day`
-            );
-          });    
     }
   },
 };
